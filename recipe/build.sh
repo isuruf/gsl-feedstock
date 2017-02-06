7caa504864a707ac2de219eea2571ef1ed30ed12
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2017-02-06 17:22:49 +0530

    Update build script so that openblas is found

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,9 @@
 
 # https://github.com/conda-forge/toolchain-feedstock/pull/11
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+# https://github.com/conda-forge/toolchain-feedstock/pull/17
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
 
 LIBS="-lopenblas -lm" ./configure --prefix=$PREFIX
 
