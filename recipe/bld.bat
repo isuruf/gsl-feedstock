92cebc68917cf88efd8ae333bee1127a6846bb8d
Filipe Fernandes <ocefpaf@gmail.com>
2019-08-20 12:08:24 -0700

    try to remove the pacthes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ set "CFLAGS=-MD"
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D BUILD_SHARED_LIBS=ON ^
-      ..
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install
