7751253334333d27e917a2d9f9d84e3b813429c4
Isuru Fernando <isuruf@gmail.com>
2019-08-21 18:43:13 -0500

    test

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,3 @@
+del /f %BUILD_PREFIX%\Library\bin\conda_build_wrapper.sh
+move %RECIPE_DIR%\conda_build_wrapper.sh %BUILD_PREFIX%\Library\bin\
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
