cd1aa032dda0b92bf544f0cf928e0dd867eb2bef
Isuru Fernando <isuruf@gmail.com>
2019-08-21 17:30:03 -0500

    Build using autotools on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,1 @@
-mkdir build
-pushd build
-
-set "CFLAGS=-MD"
-
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D BUILD_SHARED_LIBS=ON ^
-      %SRC_DIR%
-if errorlevel 1 exit 1
-
-cmake --build . --config Release --target install
-if errorlevel 1 exit 1
-
-rm %LIBRARY_LIB%\gslcblas.lib
-cp %LIBRARY_LIB%\cblas.lib %LIBRARY_LIB%\gslcblas.lib
+call %BUILD_PREFIX%\bin\run_autotools_clang_conda_build.bat
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,19 +6,24 @@ export LIBS="-lcblas -lm"
 ./configure --prefix=${PREFIX}  \
             --host=${HOST}
 
+[[ "$target_platform" == "win-64" ]] && patch_libtool
+
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
+
+ls -al "$PREFIX"/lib
+ls -al "$PREFIX"/bin
 
 rm "$PREFIX"/lib/libgslcblas.*
 CBLAS_IMPL=$(readlink $PREFIX/lib/libcblas${SHLIB_EXT})
 cp "$PREFIX/lib/${CBLAS_IMPL}" "$PREFIX/lib/libgslcblas${SHLIB_EXT}"
 
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "$target_platform" == osx* ]]; then
     ln -s "libcblas.3.dylib" "$PREFIX/lib/libgslcblas.0.dylib"
     rm "$PREFIX/lib/libcblas.3.dylib"
     touch "$PREFIX/lib/libcblas.3.dylib"
-elif [ "$(uname)" == "Linux" ]; then
+elif [[ "$target_platform" == linux* ]]; then
     ln -s "libcblas.so.3" "$PREFIX/lib/libgslcblas.so.0"
     rm "$PREFIX/lib/libcblas.so.3"
     touch "$PREFIX/lib/libcblas.so.3"
